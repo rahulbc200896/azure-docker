5db350b9486f0397163d8cc8164cf69659e53f7e
rahulbc200896 <rahulbc17@gmail.com>
2025-08-04 17:59:07 +0530

    Update application.py

--- application.py
+++ application.py
@@ -32,5 +32,5 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
 
 if __name__ == '__main__':
-    port= int(os.environ.get("PORT",8080)
+    port= int(os.environ.get("PORT",8080))
     app.run(host="0.0.0.0",port=port)
